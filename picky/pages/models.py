5a554cbdb399bbaf6d3a5e6e62de18377c485142
Wilfred Hughes <wilfred@potatolondon.com>
2012-03-02 14:35:34 +0000

    render pages as reStructuredText using docutils

--- picky/pages/models.py
+++ picky/pages/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+from docutils.core import publish_parts
+
 class Page(models.Model):
     name = models.CharField(max_length=200, unique=True)
     name_slug = models.CharField(max_length=200, unique=True)
 
     content = models.TextField()
+
+    def get_rendered_content(self):
+        """Render the reStructured text as an HTML snippet."""
+        parts = publish_parts(self.content, writer_name="html")
+        html_snippet = parts['body']
+        return html_snippet
