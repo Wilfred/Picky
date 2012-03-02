8f0e8d26a5348cda3b97ed2ab72ccc515266e155
Wilfred Hughes <me@wilfred.me.uk>
2012-03-02 01:21:45 +0000

    replacing the 'hello world' view with a view that shows all pages

--- picky/pages/views.py
+++ picky/pages/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from .models import Page
 
 
 def all_pages(request):
-    return HttpResponse('hello world')
+    template_vars = {'pages': Page.objects.order_by('name')}
+    return render_to_response("all_pages.html", template_vars,
+                              RequestContext(request))
