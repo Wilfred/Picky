fa7dee4264073395d2c16ae316c68dab027d018b
Wilfred Hughes <wilfred@potatolondon.com>
2012-03-02 14:20:31 +0000

    modifying page model to support content and a URL slug that we can query on

--- picky/pages/models.py
+++ picky/pages/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Page(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+    name_slug = models.CharField(max_length=200, unique=True)
+
+    content = models.TextField()
