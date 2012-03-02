c5e7995ae2c7abe8e0d86d9043694e5124e81a22
Wilfred Hughes <me@wilfred.me.uk>
2012-03-02 01:26:04 +0000

    building a simple Page creation page

--- picky/pages/urls.py
+++ picky/pages/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^pages/$', 'pages.views.all_pages', name='all_pages'),
+    url(r'^pages/new/$', 'pages.views.create_page', name='create_page'),
 )
--- picky/pages/views.py
+++ picky/pages/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 
 from .models import Page
+from .forms import PageForm
 
 
 def all_pages(request):
     template_vars = {'pages': Page.objects.order_by('name')}
     return render_to_response("all_pages.html", template_vars,
+                              RequestContext(request))
+
+
+def create_page(request):
+
+    if request.POST:
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('all_pages'))
+    else:
+        form = PageForm()
+
+    template_vars = {'form': form}
+
+    return render_to_response("page_edit.html", template_vars,
                               RequestContext(request))
