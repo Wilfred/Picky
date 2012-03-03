3cf43ed7b6c0c12720074dbb59b4674d4af4de55
Wilfred Hughes <me@wilfred.me.uk>
2012-03-03 16:27:26 +0000

    don't allow differently named pages with the same slug to be created

--- picky/pages/forms.py
+++ picky/pages/forms.py
@@ -1,8 +1,29 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 
 from .models import Page
+from .utils import slugify
 
 
 class PageForm(ModelForm):
     class Meta:
         model = Page
+
+    def clean_name(self):
+        """Check that this name doesn't match any other name when
+        converted to a slug.
+
+        """
+        name = self.cleaned_data['name']
+        name_slug = slugify(name)
+
+        same_slug_pages = Page.objects.filter(name_slug=name_slug)
+
+        if self.instance.id:
+            # We are editing an existing Page, so we only want to
+            # check for other pages with the same slug.
+            same_slug_pages = same_slug_pages.exclude(id=self.instance)
+
+        if same_slug_pages.exists():
+            raise ValidationError("That name clashes with another page's URL.")
+
+        return name
