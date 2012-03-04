18b6abce0b5aa7a11b5efaf5d996ed478c40eb1e
Wilfred Hughes <me@wilfred.me.uk>
2012-03-04 14:40:19 +0000

    adding a login page

--- picky/settings.py
+++ picky/settings.py
@@ -118,6 +118,8 @@ INSTALLED_APPS = (
     'users',
 )
 
+LOGIN_URL = '/login/'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
--- picky/users/urls.py
+++ picky/users/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
+
 urlpatterns = patterns('',
     url(r'^users/$', 'users.views.all_users', name='all_users'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
 )
