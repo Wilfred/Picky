1e6781b51519b66fc4e8fc1be06eec15372bfb31
Wilfred Hughes <me@wilfred.me.uk>
2012-03-06 01:01:43 +0000

    adding logout functionality

--- picky/urls.py
+++ picky/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.simple import redirect_to
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', redirect_to, {'url': '/pages/'}),
+    url(r'^$', redirect_to, {'url': '/pages/'}, name="index"),
     url(r'^', include('pages.urls')),
     url(r'^', include('users.urls')),
 
--- picky/users/views.py
+++ picky/users/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.contrib.auth import logout
 
 from .forms import UserForm
 
@@ -30,3 +31,10 @@ def create_user(request):
 
     return render_to_response("create_user.html", template_vars,
                               RequestContext(request))
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+
+    return HttpResponseRedirect(reverse('index'))
