c43287b187dba2b9ad76116f1c993b789feb3b73
Wilfred Hughes <me@wilfred.me.uk>
2012-03-06 09:58:12 +0000

    require login, even on 404 view

--- picky/site_config/views.py
+++ picky/site_config/views.py
@@ -1,7 +1,9 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def not_found(request):
     return render_to_response("site_config/404.html", {},
                               RequestContext(request))
