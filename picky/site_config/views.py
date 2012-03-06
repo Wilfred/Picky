ea2ba19fe46de4833c5b76aad750075915ab5145
Wilfred Hughes <me@wilfred.me.uk>
2012-03-06 09:49:18 +0000

    adding 404 page

--- picky/site_config/views.py
+++ picky/site_config/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+
+def not_found(request):
+    return render_to_response("site_config/404.html", {},
+                              RequestContext(request))
