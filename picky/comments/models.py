c91e23077fa9c2fc1dbe73361d75f77eb28fabec
Wilfred Hughes <me@wilfred.me.uk>
2012-12-11 00:34:12 +0000

    adding a basic model for comments, and a corresponding form

--- picky/comments/models.py
+++ picky/comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Comment(models.Model):
+    text = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    page = models.ForeignKey('pages.Page')
+
+    parent = models.ForeignKey('self', null=True)
