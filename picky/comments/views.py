30478ebe8b8186f33f20d6942b6db44496d1d99d
Wilfred Hughes <me@wilfred.me.uk>
2013-06-23 00:27:11 +0100

    minor refactoring -- don't roll-our-own POST check

--- picky/comments/views.py
+++ picky/comments/views.py
@@ -2,18 +2,17 @@ from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 from .forms import CommentForm
 from pages.models import Page
 
 
 @login_required
+@require_http_methods(["POST"])
 def new_comment(request, page_id):
     page = get_object_or_404(Page, id=page_id)
     
-    if not request.POST:
-        return HttpResponseBadRequest("Not a POST")
-
     # todo: set page, user and parent comment
     form = CommentForm(request.POST)
     comment = form.save(commit=False)
