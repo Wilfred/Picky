52d1989a43fdef4e1b16f0297a72e1b92430a20a
Wilfred Hughes <me@wilfred.me.uk>
2013-06-23 00:12:35 +0100

    access the user on the template via the request

--- picky/settings.py
+++ picky/settings.py
@@ -98,6 +98,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
+    "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
     "site_config.context_processors.include_config",
 )
