acbf1ed7fa84ea7358204224666d5a85eba07d4d
Wilfred Hughes <me@wilfred.me.uk>
2013-06-22 20:42:43 +0100

    adding a 'page actions' tab option instead of treating delete as a top level action

--- picky/pages/urls.py
+++ picky/pages/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     url(r'^pages/new/$', 'pages.views.create_page', name='create_page'),
     url(r'^pages/history/(?P<page_id>\d+)$', 'pages.views.view_page_history', name='view_page_history'),
     url(r'^pages/edit/(?P<page_id>\d+)$', 'pages.views.edit_page', name='edit_page'),
+                       
+    url(r'^pages/actions/(?P<page_id>\d+)$', 'pages.views.view_page_actions', name='page_actions'),
     url(r'^pages/delete/(?P<page_id>\d+)$', 'pages.views.delete_page', name='delete_page'),
 
     url(r'^pages/new_comment/(?P<page_id>\d+)$', 'comments.views.new_comment', name='new_comment'),
