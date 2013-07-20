592ca0798efd81fac3ea66e747351ad1878f60cd
Wilfred Hughes <me@wilfred.me.uk>
2013-07-20 10:49:26 +0100

    adding 'use strict' to our JS

--- picky/static/js/live_preview.js
+++ picky/static/js/live_preview.js
@@ -1,21 +1,26 @@
-function creoleToHtml(creoleSource) {
-    var parser = new creole({
-        linkFormat: '/page/'
+(function(){
+    "use strict";
+    
+    function creoleToHtml(creoleSource) {
+        var parser = new creole({
+            linkFormat: '/page/'
+        });
+
+        var div = document.createElement('div');
+        parser.parse(div, creoleSource);
+
+        return div.innerHTML;
+    }
+
+    function showPreview() {
+        var pageSource =  $("#id_content").val();
+
+        var $preview = $("#preview");
+        $preview.html(creoleToHtml(pageSource));
+    }
+
+    $(document).ready(function() {
+        $("#id_content").keyup(showPreview);
     });
 
-    var div = document.createElement('div');
-    parser.parse(div, creoleSource);
-
-    return div.innerHTML;
-}
-
-function showPreview() {
-    var pageSource =  $("#id_content").val();
-
-    var $preview = $("#preview");
-    $preview.html(creoleToHtml(pageSource));
-}
-
-$(document).ready(function() {
-    $("#id_content").keyup(showPreview);
-});
+})();
