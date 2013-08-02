a6e838c910fb930c120b939dc8fde84dd36034c3
Wilfred Hughes <me@wilfred.me.uk>
2013-08-02 21:25:57 +0100

    removing unused imports

--- picky/comments/views.py
+++ picky/comments/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
