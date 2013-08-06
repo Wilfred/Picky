6f39fea8d62fd1ddaeceaea695bd9b3e89ed114d
Wilfred Hughes <me@wilfred.me.uk>
2013-08-06 21:31:56 +0100

    don't allow admins to edit user's passwords

--- picky/users/forms.py
+++ picky/users/forms.py
@@ -1,22 +1,9 @@
-from django.forms import ModelForm, CharField
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 
 class UserForm(ModelForm):
-    # todo: mask password
-    # todo: confirm password
-    password = CharField()
-
     class Meta:
         model = User
         # todo: make email required
         fields = ['username', 'email']
-
-    def save(self, *args, **kwargs):
-        kwargs['commit'] = False
-        user = super(UserForm, self).save(*args, **kwargs)
-
-        user.set_password(self.cleaned_data['password'])
-        user.save()
-
-        return user
