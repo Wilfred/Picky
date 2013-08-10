3a2e5e8674a208a427aeb9ef6e88c233fecf69f4
Wilfred Hughes <me@wilfred.me.uk>
2013-08-10 15:38:26 +0100

    use Sentry in WSGI script

--- picky/wsgi.py
+++ picky/wsgi.py
@@ -5,6 +5,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picky.settings")
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = Cling(Sentry(get_wsgi_application()))
