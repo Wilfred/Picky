fa826a69056c3515cf3fb511b5b9d9852f7722a5
Wilfred Hughes <me@wilfred.me.uk>
2013-10-08 14:12:12 +0100

    tweaking the table of contents expand icon

--- picky/static/js/collapsible_toc.js
+++ picky/static/js/collapsible_toc.js
@@ -5,14 +5,14 @@
 
     if ($tocHeading.length) {
         $tocHeading.replaceWith(
-            '<h3><a id="collapse-toc">Table Of Contents <i id="collapse-icon" class="icon-minus"></i></a></h3>');
+            '<h3><a id="collapse-toc">Table Of Contents <i id="collapse-icon" class="icon-minus-sign"></i></a></h3>');
 
         $('#collapse-toc').click(function() {
             $('.table-of-contents ul').slideToggle();
 
             $('#collapse-icon')
-                .toggleClass('icon-plus')
-                .toggleClass('icon-minus');
+                .toggleClass('icon-plus-sign')
+                .toggleClass('icon-minus-sign');
         });
     }
 
