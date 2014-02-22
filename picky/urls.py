a46c938cab0f37cd4b02a0ea90f02c7d2c140da9
Wilfred Hughes <me@wilfred.me.uk>
2014-02-22 20:39:39 +0000

    Refactoring URLs to group by app.

--- picky/urls.py
+++ picky/urls.py
@@ -4,21 +4,24 @@ from django.conf.urls.defaults import patterns, url, include
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'pages.views.index', name='index'),
-    url(r'^all_pages/$', 'pages.views.all_pages', name='all_pages'),
-    url(r'^all_pages/urls/$', 'pages.views.all_page_names', name='all_page_names'),
-    url(r'^new_page/$', 'pages.views.create_page', name='create_page'),
-    url(r'^new_page/(?P<page_slug>[^/]+)$', 'pages.views.create_page', name='create_page'),
+urlpatterns = patterns('pages.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^all_pages/$', 'all_pages', name='all_pages'),
+    url(r'^all_pages/urls/$', 'all_page_names', name='all_page_names'),
+    url(r'^new_page/$', 'create_page', name='create_page'),
+    url(r'^new_page/(?P<page_slug>[^/]+)$', 'create_page', name='create_page'),
                        
-    url(r'^page/(?P<page_slug>[^/]+)/$', 'pages.views.view_page', name='view_page'),
-    url(r'^page/(?P<page_slug>[^/]+)/edit/$', 'pages.views.edit_page', name='edit_page'),
-    url(r'^page/(?P<page_slug>[^/]+)/changes/$', 'pages.views.view_page_history', name='view_page_history'),
+    url(r'^page/(?P<page_slug>[^/]+)/$', 'view_page', name='view_page'),
+    url(r'^page/(?P<page_slug>[^/]+)/edit/$', 'edit_page', name='edit_page'),
+    url(r'^page/(?P<page_slug>[^/]+)/changes/$', 'view_page_history', name='view_page_history'),
 
-    url(r'^page/(?P<page_slug>[^/]+)/actions/$', 'pages.views.view_page_actions', name='view_page_actions'),
-    url(r'^page/(?P<page_slug>[^/]+)/actions/delete/$', 'pages.views.delete_page', name='delete_page'),
+    url(r'^page/(?P<page_slug>[^/]+)/actions/$', 'view_page_actions', name='view_page_actions'),
+    url(r'^page/(?P<page_slug>[^/]+)/actions/delete/$', 'delete_page', name='delete_page'),
 
-    url(r'^page/(?P<page_slug>[^/]+)/comments/$', 'pages.views.view_page_comments', name='view_page_comments'),
+    url(r'^page/(?P<page_slug>[^/]+)/comments/$', 'view_page_comments', name='view_page_comments'),
+)
+
+urlpatterns += patterns('',
     url(r'^page/(?P<page_slug>[^/]+)/comments/new/$', 'comments.views.new_comment', name='new_comment'),
     url(r'^page/(?P<page_slug>[^/]+)/comments/new/(?P<parent_id>\d+)/$', 'comments.views.new_comment', name='new_comment'),
 
