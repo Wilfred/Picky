3a330b19e9907b7dc4f101ffff0a95a2d2ee103a
Wilfred Hughes <me@wilfred.me.uk>
2020-01-12 00:01:05 +0000

    Configure logging to write to stdout

--- picky/settings.py
+++ picky/settings.py
@@ -184,37 +184,25 @@ HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-            'filters': ['require_debug_false'],
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'propagate': True,
+        'console': {
+            'class': 'logging.StreamHandler',
         },
     },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
     },
 }
 
 INTERNAL_IPS = ('127.0.0.1',)
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 WSGI_APPLICATION = "picky.wsgi.application"
 
