5998976bb23b3ebd926ff1eb87268dff374c7e32
Wilfred Hughes <me@wilfred.me.uk>
2020-01-12 16:10:38 +0000

    Tweak logging format

--- picky/settings.py
+++ picky/settings.py
@@ -187,9 +187,15 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'console',
         },
     },
     'loggers': {
